ce206ae60fd9dcea2208a0642137312077580e52
GejjalaLakshmipathi <85888123+GejjalaLakshmipathi@users.noreply.github.com>
2023-06-05 16:29:06 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import random
+from datetime import datetime
+
 import databutton as db
 import streamlit as st
 from twilio.rest import Client
@@ -8,10 +10,15 @@ TARGET_DATAFRAME_KEY = "registrations"
 # Twilio account details
 account_sid = 'your_account_sid'
 auth_token = 'your_auth_token'
+twilio_number = 'your_twilio_number'
 client = Client(account_sid, auth_token)
 
-with st.form("register_form"):
-    st.header("Register")
+@st.cache_data()
+def get_data():
+    return {}
+
+def register_form():
+    data = get_data()
     first_name = st.text_input("First Name")
     email = st.text_input("Email")
     mobile_number = st.text_input("Mobile Number")
@@ -24,10 +31,13 @@ with st.form("register_form"):
         # Send the OTP to the provided mobile number using Twilio
         message = client.messages.create(
             body=f"Your OTP is {otp}",
-            from_='your_twilio_number',
+            from_=twilio_number,
             to=mobile_number
         )
         st.success("OTP sent to your mobile number")
+
+        data["otp"] = otp
+        data["mobile_number"] = mobile_number
 
     # OTP input field
     otp_input = st.text_input("Enter OTP")
@@ -35,15 +45,16 @@ with st.form("register_form"):
     # Verify OTP button
     if st.button("Verify OTP"):
         # Check if the OTP matches the input
-        if str(otp) == otp_input:
+        if str(data.get("otp")) == otp_input:
             st.success("OTP verified")
+            data["verified"] = True
         else:
             st.error("Invalid OTP")
 
     # Submit button
     submitted = st.form_submit_button("Submit")
 
-    if submitted:
+    if submitted and data.get("verified"):
         st.success(f"Registration successful! Welcome, {first_name}!")
 
         db.storage.dataframes.add(
@@ -56,3 +67,9 @@ with st.form("register_form"):
             },
         )
 
+def main():
+    st.header("Register")
+    register_form()
+
+if __name__ == "__main__":
+    main()
